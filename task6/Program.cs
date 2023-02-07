092ab137815cc1ceaa00c6c96186f102c3311ab3
nyshashina <nyshashina@gmail.com>
2023-02-07 23:05:51 +0300

    task6 was done

---
+++ task6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Таблица четных кубов чисел от 1 до " + numberA);
+for(int i = 2; i <= numberA; i++)
+{
+    Console.WriteLine(i + " " + i * i * i);
+    i = i + 1;
+}
