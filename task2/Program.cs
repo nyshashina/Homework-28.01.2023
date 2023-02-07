eec92c0a51ba4f03e8c76f6e138883ff349a51ca
nyshashina <nyshashina@gmail.com>
2023-02-07 22:35:10 +0300

    task2 was done

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine() ?? "");
+
+int sum = 0;
+for(int i = 1; i <= numberA; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine("Сумма чисел от 1 до " + numberA + " равна " + sum);
