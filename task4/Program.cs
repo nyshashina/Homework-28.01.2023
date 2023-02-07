f3b488bcc3c56e7379d09ab3bd5fd69919124240
nyshashina <nyshashina@gmail.com>
2023-02-07 22:52:01 +0300

    task4 was done

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine() ?? "");
+
+int sum = 0;
+int numberB = numberA;
+while (numberB > 0)
+{
+    sum = sum + numberB % 10;
+    numberB = numberB / 10;
+}
+Console.WriteLine("Сумма цифр в числе " + numberA + " равна " + sum);
