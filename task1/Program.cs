b5e821da474b5e9bdbedecbaec0a571a4bc9ea42
nyshashina <nyshashina@gmail.com>
2023-02-07 22:24:50 +0300

    task1 was done

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти кубы чисел от 1 до N
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Таблица кубов чисел от 1 до " + numberA);
+for(int i = 1; i <= numberA; i++)
+{
+    Console.WriteLine(i + " " + i * i * i);
+}
