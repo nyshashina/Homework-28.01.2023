8bd2fc53bc2447cf7be19d27914997a45e30f7a0
nyshashina <nyshashina@gmail.com>
2023-02-07 22:41:59 +0300

    task3 was done

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Возведите число А в натуральную степень B, используя цикл
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите степень: ");
+int numberB = int.Parse(Console.ReadLine() ?? "");
+
+int degree = 1;
+for(int i = 1; i <= numberB; i++)
+{
+    degree = degree * numberA;
+}
+Console.WriteLine("Число " + numberA + ", возведенное в степень " + numberB + ", равно " + degree);
