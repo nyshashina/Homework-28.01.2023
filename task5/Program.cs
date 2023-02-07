48685b5d3465644f591645e38d53b50eb743a296
nyshashina <nyshashina@gmail.com>
2023-02-07 22:58:42 +0300

    task5 was done

---
+++ task5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine() ?? "");
+
+double mult = 1;
+for(int i = 1; i <= numberA; i++)
+{
+    mult = mult * i;
+}
+Console.WriteLine("Произведение чисел от 1 до " + numberA + " равно " + mult);
