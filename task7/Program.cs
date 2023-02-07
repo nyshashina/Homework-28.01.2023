8e004013078b62b7993a805f40a8eae4db5a0f83
nyshashina <nyshashina@gmail.com>
2023-02-07 23:15:33 +0300

    task7 was done

---
+++ task7/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int[] arr = new int[8];
+foreach(int el in arr){
+    arr[el] = new Random().Next(0,2);
+    Console.Write(arr[el] + " ");
+}
