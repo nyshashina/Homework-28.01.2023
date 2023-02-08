430d338cc939078dc1beb746fd37f923068681c4
nyshashina <nyshashina@gmail.com>
2023-02-08 17:42:07 +0300

    task13 was done

---
+++ task13/Program.cs
@@ -0,0 +1,21 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+double[] arr = new double[10];
+for (int i = 0; i < 10; i++){
+    arr[i] = new Random().Next(-9,10);
+    Console.Write(arr[i] + " ");
+} 
+Console.WriteLine("");
+
+double min = arr[0];
+double max = arr[0];
+for (int i = 1; i < arr.Length; i++){
+    if (arr[i] > max){
+        max = arr[i];
+    }
+    else if(arr[i] < min){
+        min = arr[i];
+    }
+}
+Console.Write("Разница между максимальным и минимальным элементами равна ");
+Console.Write(max - min);
