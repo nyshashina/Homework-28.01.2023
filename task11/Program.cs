845f7b1348f0708d1aff5456c4f8a9fe5fcd907d
nyshashina <nyshashina@gmail.com>
2023-02-08 16:31:30 +0300

    task11 was done

---
+++ task11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+Console.WriteLine("Задан массив:");
+
+int sum = 0;
+int[] arr = new int[10];
+for (int i = 0; i < 10; i++){
+    arr[i] = new Random().Next(0,10);
+    Console.Write(arr[i] + " ");
+    if(i % 2 != 0){
+        sum = sum + arr[i];
+    }
+}
+Console.WriteLine("");
+Console.WriteLine("Сумма чисел одномерного массива, стоящих на нечетной позиции, равна " + sum);
